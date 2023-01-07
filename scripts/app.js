bf9216d9f71e02b81f458ac392c92b94e2dff366
saraltr <saralatorre97@gmail.com>
2023-01-08 00:36:33 +0100

    assignment portal page created

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,30 @@
 const h1 = document.querySelector("h1");
 h1.textContent = "Sara Latorre";
+
+const year = document.querySelector("#currentyear");
+
+try {
+	const options = {
+		year: "numeric"
+	};
+	year.innerHTML = `${new Date().toLocaleDateString("en-US", options)}</span>`;
+} catch (e) {
+	console.log("Error with code or your browser does not support Locale");
+}
+
+const date = document.querySelector("#currentdate");
+
+try {
+	const options = {
+		month: "2-digit",
+		day: "2-digit",
+		year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+	};
+	date.innerHTML = `${new Date().toLocaleDateString("en-US", options)}</span>`;
+} catch (e) {
+	console.log("Error with code or your browser does not support Locale");
+}
+
