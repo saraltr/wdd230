b364d826a9abcceee640c9babd89202492e69612
saraltr <saralatorre97@gmail.com>
2023-01-29 07:44:53 +0100

    medium vp added

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -6,3 +6,41 @@ function toggleMenu(){
 const x = document.getElementById("hamburgerBtn");
 
 x.addEventListener("click", toggleMenu);
+
+document.querySelector(
+	"#lastModified"
+).textContent = `Last Modification: ${document.lastModified}`;
+
+
+//Get date
+
+const daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+const fulldate = `${dayName} ${d.getDate()}, ${monthName} ${year}`;
+
+document.querySelector("#currentdate").textContent = fulldate;
