ca38983e84917b2ed4baea080b0d9fdb9e6771a4
saraltr <saralatorre97@gmail.com>
2023-02-20 19:06:58 +0100

    discover page added

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -44,5 +44,18 @@ if (day === "Monday" || day === "Tuesday"){
 		banner.style.display = "none"
 	}
 	)
-
 }
+const visitsDisplay = document.querySelector("#visits");
+
+let numVisits = Number(window.localStorage.getItem("visits-ls")); 
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
