57f1751877f04bc8131feab6e359d7bc2c29a560
saraltr <saralatorre97@gmail.com>
2023-03-19 07:52:15 +0100

    weather api updated

--- chamber/scripts/weatherapi.js
+++ chamber/scripts/weatherapi.js
@@ -15,7 +15,6 @@ async function apiFetch() {
     if (response.ok) {
       const data = await response.json();
       displayResults(data);
-      console.log(data)
     } else {
       throw Error(await response.text());
     }
