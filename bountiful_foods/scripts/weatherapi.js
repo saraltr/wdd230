def3c6f8ca2bf8cb798de31679eadfd8fdef4f50
saraltr <saralatorre97@gmail.com>
2023-04-09 09:10:11 +0200

    footer and contrast updated

--- bountiful_foods/scripts/weatherapi.js
+++ bountiful_foods/scripts/weatherapi.js
@@ -1,18 +1,20 @@
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Carlsbad&appid=73cb8ad5fb9a00832e5798812757f4d3&units=imperial";
+const weather = "https://api.openweathermap.org/data/2.5/weather?q=Carlsbad&appid=73cb8ad5fb9a00832e5798812757f4d3&units=imperial";
 
 const forecast = "https://api.openweathermap.org/data/2.5/forecast?q=Carlsbad&appid=cfe6dd84498951e1d5f24b029c0cffc0&units=imperial";
 
 const currentTemp = document.querySelector("#currentTemp");
 const weatherIcon = document.querySelector("#weatherIcon");
+const humidity = document.querySelector("#humidity");
 const captionDesc = document.querySelector("figcaption");
+const tomorrowsTemp = document.querySelector("#tmr");
 
-async function apiFetch() {
+async function apiFetch(url) {
   try {
     const response = await fetch(url);
     if (response.ok) {
       const data = await response.json();
       displayResults(data);
-        // degug console.log(data)
+      console.log(data)
     } else {
       throw Error(await response.text());
     }
@@ -20,11 +22,12 @@ async function apiFetch() {
     console.log(error);
   }
 }
-apiFetch();
+apiFetch(weather);
+apiFetch(forecast);
 
 function displayResults(weatherData){
     if (weatherData.main) {
-        currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)} °F</strong>`;
+        currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}°F</strong>`;
         const iconScr = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
 
         weatherIcon.setAttribute("src", iconScr);
@@ -37,7 +40,13 @@ function displayResults(weatherData){
         .join(" ");
         weatherIcon.setAttribute("alt", description);
         captionDesc.innerHTML = `<strong>${description}</strong>`;
-
+        humidity.textContent = weatherData.main.humidity;
+        // if(weatherData.list){
+        // const tomorrow = weatherData.list[9];
+        // tomorrowsTemp.innerHTML = `<strong>${tomorrow.main.temp.toFixed(0)} °F</strong>`;
+        // can't figure out how to make it work
+        // }
+        
     }
 
     
