fe680df81d2fb92a8b6ecaf64e32b90e7752bb5c
saraltr <saralatorre97@gmail.com>
2023-04-09 07:52:37 +0200

    footer updated

--- bountiful_foods/scripts/app.js
+++ bountiful_foods/scripts/app.js
@@ -3,6 +3,47 @@ function toggleMenu() {
   document.getElementById("hamburgerBtn").classList.toggle("open");
 }
 
-const x = document.getElementById("hamburgerBtn");
+const btn = document.getElementById("hamburgerBtn");
 
-x.addEventListener("click", toggleMenu);
+btn.addEventListener("click", toggleMenu);
+
+document.querySelector(
+  "#lastModified"
+).textContent = `Last Modification: ${document.lastModified}`;
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let d = new Date();
+let year = d.getFullYear();
+let time = d.getTime();
+let day = days[d.getDay()];
+let month = months[d.getMonth()];
+let date = d.getDate();
+
+const fulldate = `${day}, ${date} ${month} ${year} `;
+
+document.querySelector("#year").textContent = year;
+
