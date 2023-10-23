bf3f56733ee6edf66ab090b2cf709d9a60dc153b
sara latorre <saralatorre97@gmail.com>
2023-10-23 23:49:54 +0200

    new mobile view test

--- bountiful_foods/scripts/app.js
+++ bountiful_foods/scripts/app.js
@@ -52,12 +52,126 @@ const heroImg = "images/hero.png";
 const section = document.getElementById("hero-section");
 const placeholder = document.getElementById("hero-placeholder");
 
-// Create a new image element and set the source to the actual image
+// create a new image element and set the source to the actual image
 const img = new Image();
-img.src = heroImg; // Set the actual image source
+img.src = heroImg; // set the actual image source
 
-// When the actual image is loaded, replace the section background image
+// when the actual image is loaded, replace the section background image
 img.onload = function () {
   section.style.backgroundImage = `url(${img.src})`;
   placeholder.style.display = "none"; // Hide the placeholder div
+};
+
+// drink of the day
+const specials = "api/specials.json";
+const drinksDiv = document.querySelector(".drinkContainer");
+
+
+async function getData() {
+  const response = await fetch(specials);
+  const data = await response.json();
+  console.log(data.fruitDrinks[5]);
+  displayDrinkOfTheDay(data.fruitDrinks[0]);
+}
+getData();
+
+const displayDrinkOfTheDay = (drink) => {
+
+  // drinks.forEach((drink) => {
+
+  // drinks of the day img
+    const img = document.createElement("img");
+    img.src = drink.image;
+    img.alt = drink.name;
+
+    // drinks content div (description, price, rating, nb of votes)
+    const drinkCont = document.createElement("div");
+    drinkCont.classList.add("drinkText");
+
+    // title
+    const drinkTitle = document.createElement("h3");
+    drinkTitle.textContent = drink.name
+    drinkTitle.style.color = "#d14408";
+
+    // description
+    const description = document.createElement("p");
+    description.textContent = drink.description;
+    description.style.color = "black";
+
+    // rating stars
+    const rating = drink.clientRating;
+
+    // rating Div for the stars
+    const ratingDiv = document.createElement("div");
+    ratingDiv.classList.add("rating");
+    const ratingsNumb = document.createElement("p");
+    ratingsNumb.innerHTML = `${drink.numberOfVotes} ratings`;
+    ratingsNumb.style.fontSize = "smaller";
+
+    // function to generate star images based on the rating
+    const displayRatingStars = (rating, div) => {
+
+      const totalStars = 5; // Total number of stars
+      const fullStars = Math.floor(rating); // number of full stars
+      const hasHalfStar = rating % 1; // get the fractional part of the rating
+
+      for (let i = 0; i < fullStars; i++) {
+        const starImage = document.createElement("img");
+        starImage.src = "images/filled.png"; // use the filled star image
+        starImage.alt = "Filled Star";
+        div.appendChild(starImage);
+      }
+
+      if (hasHalfStar) {
+        const starImage = document.createElement("img");
+        starImage.src = "images/middle.png"; // use the half-filled star image
+        starImage.alt = "Half-Filled Star";
+        div.appendChild(starImage);
+      }
+
+      for (let i = 0; i < totalStars - fullStars - (hasHalfStar ? 1 : 0); i++) {
+        const starImage = document.createElement("img");
+        starImage.src = "images/empty.png"; // use the empty star image for the remaining stars
+        starImage.alt = "Empty Star";
+        div.appendChild(starImage);
+      }
+    }
+
+    displayRatingStars(rating, ratingDiv);
+
+    const priceDiv = document.createElement("div");
+    priceDiv.classList.add("priceDiv");
+    // normal price
+    const price = document.createElement("p");
+    price.innerHTML = `$${drink.regularPrice}`;
+    price.style.color = "#d14408"
+    price.style.fontSize = "smaller";
+    price.style.textDecoration = "line-through";
+
+    // special day price
+    const newPrice = document.createElement("p");
+    newPrice.innerHTML = `$${drink.specialPrice}`;
+    newPrice.style.color = "#289946";
+    newPrice.style.fontSize = "larger";
+
+    // price div
+    priceDiv.appendChild(price);
+    priceDiv.appendChild(newPrice);
+
+    const card = document.createElement("img");
+    card.src = "images/cart.png";
+    card.alt = "cart icon";
+    card.classList.add("cart");
+
+    // add els to the content div
+    drinkCont.append(description);
+    drinkCont.appendChild(ratingDiv);
+    drinkCont.appendChild(priceDiv);
+    drinkCont.appendChild(card);
+
+    // add els to the div
+    drinksDiv.append(img);
+    drinksDiv.append(drinkTitle);
+    drinksDiv.append(drinkCont);
+  // })
 };
